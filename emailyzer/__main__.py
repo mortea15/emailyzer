8a63a3308a5810c222f99fc2073026c0807056cf
mortea15 <m.amundsen@pm.me>
2020-01-30 15:51:36 +0100

    added missing param to help message

--- emailyzer/__main__.py
+++ emailyzer/__main__.py
@@ -13,13 +13,14 @@ current = os.path.realpath(os.path.dirname(__file__))
 
 
 INDENT = '  '
-HELPMSG = f'''usage: emailyzer [-h] (-e .EML FILE | -m .MSG FILE | -f FILE) [-b] [-a] [-r] [-t] [-o] [-u] [-sa] [-v]
+HELPMSG = f'''usage: emailyzer (-e .EML FILE | -m .MSG FILE | -f FILE) [-b] [-h] [-a] [-r] [-t] [-o] [-u] [-sa] [-v]
 
     {INDENT * 1}--help                  {INDENT * 2}Print this message
     {INDENT * 1}-e, --eml               {INDENT * 2}Parse a .eml file
     {INDENT * 1}-m, --msg               {INDENT * 2}Parse a .msg file
     {INDENT * 1}-f, --file              {INDENT * 2}Parse a file (must be either .msg or .eml format)
     {INDENT * 1}-b, --body              {INDENT * 2}Print the body of the email
+    {INDENT * 1}-h, --html              {INDENT * 2}Print the HTML content of the email
     {INDENT * 1}-a, --attachments       {INDENT * 2}Print the attachments of the email
     {INDENT * 1}-r, --headers           {INDENT * 2}Print the headers of the email
     {INDENT * 1}-t, --to                {INDENT * 2}Print the to of the email
@@ -51,7 +52,7 @@ def main():
         sys.exit(0)
 
     for opt, arg in opts:
-        if opt in ('--help'):
+        if opt == '--help':
             print(HELPMSG)
             sys.exit(0)
         elif opt in ('-v', '--verbose'):
