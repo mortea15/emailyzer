04c03e581197771672c8ade9668e5aa9924595a4
mortea15 <m.amundsen@pm.me>
2020-03-12 13:35:27 +0100

    gotta catch em all

--- emailyzer/emailyzer.py
+++ emailyzer/emailyzer.py
@@ -2,22 +2,31 @@
 # -*- coding: utf-8 -*-
 
 from emailyzer.classes.email import Email
-
+from emailyzer.helpers.logger import rootLogger as logger
 
 def from_msg(filepath):
-    return Email.from_msg(filepath)
+    try:
+        return Email.from_msg(filepath)
+    except Exception as e:
+        logger.error(e)
+        return Email.from_msg(filepath)
 
 
 def from_eml(filepath):
-    return Email.from_eml(filepath)
+    try:
+        return Email.from_eml(filepath)
+    except Exception as e:
+        logger.error(e)
+        return Email.from_eml(filepath)
 
 
 def from_file(filepath):
     try:
         return from_msg(filepath)
     except Exception as e:
+        logger.error(e)
         pass
     try:
         return from_eml(filepath)
     except Exception as e:
-        pass
+        logger.error(e)
